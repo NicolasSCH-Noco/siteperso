d2a1fd2eeb6e8dd3b0e4ec1c652cea29608a7002
Bibadel <nicolasscharre@outlook.fr>
2022-03-28 23:04:14 +0200

    Configuration Articles (Model + Relation)

--- database/migrations/2022_03_28_205428_create_articles_table.php
+++ database/migrations/2022_03_28_205428_create_articles_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
+            $table->string('title')->default("Première article");
+            $table->text('body')->default("Première article qui ma parole, n'a aucun sens .. Mais on fait avec");
+            $table->foreignId("user_id")->constrained()->onDelete("cascade");
             $table->timestamps();
         });
     }
