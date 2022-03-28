836d782e5bbeaac9bea335af56cda3528270a97b
Bibadel <nicolasscharre@outlook.fr>
2022-03-28 22:47:26 +0200

    Création Classe Fichier +a +r

---
+++ app/Http/Controllers/FichierController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Fichier;
+use App\Http\Requests\StoreFichierRequest;
+use App\Http\Requests\UpdateFichierRequest;
+
+class FichierController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreFichierRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreFichierRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Fichier  $fichier
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Fichier $fichier)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Fichier  $fichier
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Fichier $fichier)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateFichierRequest  $request
+     * @param  \App\Models\Fichier  $fichier
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateFichierRequest $request, Fichier $fichier)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Fichier  $fichier
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Fichier $fichier)
+    {
+        //
+    }
+}
