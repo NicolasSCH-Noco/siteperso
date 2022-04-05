57d0d937c39125bc1d9a34eed7e8e27277565ba7
Bibadel <nicolasscharre@outlook.fr>
2022-04-05 09:48:00 +0200

    Mise à jour de la fonction show Controller : Article

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -59,7 +59,7 @@ class ArticleController extends Controller
      */
     public function show(Article $article)
     {
-        //
+        return view('article.show', compact('article'));
     }
 
     /**
